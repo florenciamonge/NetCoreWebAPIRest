8ba7772895c4331d8ed321755f04b30046725aba
Florencia Monge <mongeflorencia94@gmail.com>
2021-07-09 21:28:42 +0200

    Primer commit de web api

---
+++ Data/WebAPIbooksContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIbooks.Models;
+
+namespace WebAPIbooks.Data
+{
+    public class WebAPIbooksContext : DbContext
+    {
+        public WebAPIbooksContext (DbContextOptions<WebAPIbooksContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIbooks.Models.Book> Book { get; set; }
+    }
+}
